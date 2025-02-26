3f8529141f44e4bc8b75c685d3f95bb5b4c250ab
DESKTOP-R5VO05B\SJ_JM <kjmopmjk@gmail.com>
2025-02-27 01:52:15 +0900

    init

---
+++ database/migrations/2025_02_26_150348_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id()
+                ->comment('상품id');
+            $table->string('name',255)
+                ->comment('상품명');
+            $table->bigInteger('price')
+                ->comment('상품금액');
+            $table->foreignId('user_id')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
