05b874a7678936c75e526f72c6404794a095b194
DESKTOP-R5VO05B\SJ_JM <kjmopmjk@gmail.com>
2025-03-22 00:33:16 +0900

    apply log

--- app/Services/Admin/AdminMenuService.php
+++ app/Services/Admin/AdminMenuService.php
@@ -3,15 +3,26 @@
 namespace App\Services\Admin;
 
 use App\Repositories\Admin\AdminMenuRepository;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 class AdminMenuService {
     protected $adminMenuRepository;
+    protected $logger;
 
     public function __construct(AdminMenuRepository $adminMenuRepository) {
         $this->adminMenuRepository = $adminMenuRepository;
+
+        $this->logger = new Logger('AdminMenuService');
+        $this->logger->pushHandler(new StreamHandler(storage_path('logs/laravel.log'), Logger::INFO));
     }
 
     public function getMenuList() {
-        return $this->adminMenuRepository->getMenuList();
+
+        $menus = $this->adminMenuRepository->getMenuList();
+
+        $this->logger->info('===로그찍기===');
+
+        return $menus;
     }
 }
